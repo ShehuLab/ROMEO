828812462dc2d5c2ba92e214be8543ac60382da9
Kevin Molloy <kmolloy717@gmail.com>
2018-11-12 09:51:40 -0500

    Add ROMEO.cpp to distribution.

---
+++ src/Programs/ROMEO.cpp
@@ -0,0 +1,44 @@
+#include "Utils/DLHandler.hpp"
+#include "Utils/PseudoRandom.hpp"
+#include "Utils/Logger.hpp"
+#include "Utils/Misc.hpp"
+
+using namespace Antipatrea;
+
+/**
+ *@author Erion Plaku, Amarda Shehu
+ *@brief Program to run other programs.
+ *@remarks
+ *  - First argument is the name of the program that should be run.
+ *  - The other arguments are arguments to that program.
+ *  - ROMEO looks for the name of the program in the list of the loaded symbols.
+ *    - if it finds it, it runs the program passing along the arguments.
+ *    - otherwise, it displays a warning message.
+ *  - In order for ROMEO to be able to find the program, it should be of the form
+ *   <center> <tt>extern "C" int ProgramName(int argc, char **argv)</tt></center>
+ *   where <tt>ProgramName</tt> could be an valid function name.
+ */
+int main(int argc, char **argv)
+{
+    typedef int (*MainFcn) (int, char **);
+
+    OutputFormat(Logger::m_out);
+
+    RandomSeed();
+
+    if(argc < 2)
+        Logger::m_out << "usage: ROMEO <program_name> [list_of_program_args]" << std::endl;
+    else
+    {
+        MainFcn fcn = (MainFcn) Antipatrea::DLHandler::GetSymbol(argv[1]);
+        if(fcn)
+        {
+            Logger::m_out << "Running program <" << argv[1] << ">" << std::endl << std::endl;
+            return fcn(argc - 1, &(argv[1]));
+        }
+        else
+            Logger::m_out << "Program <" << argv[1] << "> not found" << std::endl;
+    }    
+
+    return 0;
+}
